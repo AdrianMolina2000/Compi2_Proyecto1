ec954b8f0c97b9fa74261f833b20aae43afc2e14
Adrian Molina <adriansmc@gmail.com>
2021-12-07 22:40:40 -0600

    Prueba Interfaz

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+let parser = require('../grammar');
+
+   
+var entrada = document.getElementById("editor1").value;
+
+var editor = CodeMirror.fromTextArea(document.getElementById('editor1'), {
+    mode: "javascript",
+    lineNumbers: true,
+    theme: "material-darker",
+    smartIndent: true,
+});
+editor.save()
+
+var editor2 = CodeMirror.fromTextArea(document.getElementById('editor2'), {
+    mode: "javascript",
+    lineNumbers: true,
+    theme: "material-darker",
+    smartIndent: true,
+    readOnly: "nocursor"
+});
+editor2.save()
+
+
+
+const ejecutar = document.getElementById("interpretar");
+
+ejecutar.addEventListener('click', () => {
+    let entrada = editor.getValue();
+
+    alert('Juan Diego gey');
+    console.log(entrada);
+})
